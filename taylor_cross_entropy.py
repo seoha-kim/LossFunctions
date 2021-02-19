753a1c47c94305be37150c78bfe984ad956301d7
Seoha Kim <46925617+seoha-kim@users.noreply.github.com>
2021-02-19 18:14:15 +0900

    2021-2-19 updates

---
+++ taylor_cross_entropy.py
@@ -0,0 +1,40 @@
+from label_smoothing_loss import *
+
+class TaylorSoftmax(nn.Module):
+    '''
+    This is the autograd version
+    '''
+    def __init__(self, dim=1, n=2):
+        super(TaylorSoftmax, self).__init__()
+        assert n % 2 == 0
+        self.dim = dim
+        self.n = n
+
+    def forward(self, x):
+        '''
+        usage similar to nn.Softmax:
+            >>> mod = TaylorSoftmax(dim=1, n=4)
+            >>> inten = torch.randn(1, 32, 64, 64)
+            >>> out = mod(inten)
+        '''
+        fn = torch.ones_like(x)
+        denor = 1.
+        for i in range(1, self.n+1):
+            denor *= i
+            fn = fn + x.pow(i) / denor
+        out = fn / fn.sum(dim=self.dim, keepdims=True)
+        return out
+
+class TaylorCrossEntropyLoss(nn.Module):
+    def __init__(self, target_size, n=2, ignore_index=-1, reduction='mean', smoothing=0.05):
+        super(TaylorCrossEntropyLoss, self).__init__()
+        assert n % 2 == 0
+        self.taylor_softmax = TaylorSoftmax(dim=1, n=n)
+        self.reduction = reduction
+        self.ignore_index = ignore_index
+        self.lab_smooth = LabelSmoothingLoss(target_size, smoothing=smoothing)
+
+    def forward(self, logits, labels):
+        log_probs = self.taylor_softmax(logits).log()
+        loss = self.lab_smooth(log_probs, labels)
+        return loss
